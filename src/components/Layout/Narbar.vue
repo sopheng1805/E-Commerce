4f985b376518ab1ca8074a5ae07ddc57b4a93ded
sopheng1805 <sreykk545@gmail.com>
2026-05-20 12:33:31 +0700

    update

--- src/components/Layout/Narbar.vue
+++ src/components/Layout/Narbar.vue
@@ -1,5 +1,5 @@
 <template>
-    <header class="sticky top-0 z-index">
+    <header class="sticky top-0 z-index bg-pink-200">
         <div v-if="isLoading" class="fixed inset-0 z-[9999] bg-black flex flex-col justify-center items-center">
             <!-- Spinner -->
             <div class="w-16 h-16 border-4 border-blue-500 border-t-transparent rounded-full animate-spin"></div>
